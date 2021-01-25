d5b94d8ea6781bd900ba11c11dad4d50f797dbd5
Esra Ünal <esraunaal@gmail.com>
2021-01-26 00:37:49 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string> () ;
+            myDictionary.Add(1 , "Muz");
+            myDictionary.Add(2 , "Armut");
+            myDictionary.Add(3 , "Portakal" );
+            myDictionary.Add(4 , "Kereviz");
+            myDictionary.Add(5 , "Ispanak");
+            myDictionary.Show();
+
+        }
+    }
+}
